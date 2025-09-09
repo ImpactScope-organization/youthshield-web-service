48fa5cde91386b0e94037e75149bd27ec983561c
balazs-impact-scope <balazs@impactscope.com>
2025-09-09 08:31:28 +0200

    postid

--- src/app/influencers/[influencerId]/posts/[postId]/page.tsx
+++ src/app/influencers/[influencerId]/posts/[postId]/page.tsx
@@ -13,8 +13,41 @@ export default async ({ params }: Props) => {
   return (
     <PageContainer>
       <Flex gap="4" direction="column">
-        <Heading>{misleadingPost.text.original}</Heading>
-        <Grid gap="4" columns="3"></Grid>
+        <Heading>Original Post</Heading>
+        <p>{misleadingPost?.text.original}</p>
+        {misleadingPost?.text.ocr && (
+          <>
+            <Heading size="3">Image Text (OCR)</Heading>
+            <p>{misleadingPost.text.ocr}</p>
+          </>
+        )}
+        <Flex gap="4" direction="column">
+          <Heading>Analysis</Heading>
+          <Flex gap="4" direction="column">
+            <div>
+              <strong>Identified Issue:</strong>
+              <p>{misleadingPost?.analysis.identifiedIssue}</p>
+            </div>
+            <div>
+              <strong>Official Recommendation:</strong>
+              <p>{misleadingPost?.analysis.officialRecommendation}</p>
+            </div>
+            <div>
+              <strong>Contradiction or Inconsistency:</strong>
+              <p>{misleadingPost?.analysis.contradictionOrInconsistency}</p>
+            </div>
+            <div>
+              <a
+                href={misleadingPost?.analysis.linkToReliableSource}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline"
+              >
+                Source
+              </a>
+            </div>
+          </Flex>
+        </Flex>
       </Flex>
     </PageContainer>
   )
