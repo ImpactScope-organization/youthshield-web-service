2db7a17d9af826b52d112e2309e95d7426017a56
balazs-impact-scope <balazs@impactscope.com>
2025-09-09 07:05:35 +0200

    add id query

--- src/app/influencers/[influencer]/page.tsx
+++ src/app/influencers/[influencer]/page.tsx
@@ -1,5 +1,7 @@
 import { PageContainer } from '@/components/common/PageContainer'
+import { serverClient } from '@/trpc/client/server-client'
 
-export default () => {
-  return <PageContainer>hello world</PageContainer>
+export default async () => {
+  const influencer = await serverClient.influencers.get({ id: '68b9722b72066ed02955cb84'})
+  return <PageContainer>{influencer?.name}</PageContainer>
 }
--- src/trpc/server/queries/influencers.ts
+++ src/trpc/server/queries/influencers.ts
@@ -1,6 +1,7 @@
 import { publicProcedure } from '@/trpc/server'
 import { dbConnect } from '@/db/mongoose'
 import { InfluencerModel, TInfluencer } from '@/models/Influencer'
+import { z } from 'zod'
 
 export const influencers = {
   all: publicProcedure.query(async () => {
@@ -17,5 +18,12 @@ export const influencers = {
       }
     ])
     return influencers
-  })
+  }),
+  get: publicProcedure.input(z.object({
+    id: z.string()
+  })).query(async (opts) => {
+    await dbConnect()
+    const influencer: TInfluencer | null = await InfluencerModel.findById(opts.input.id)
+    return influencer
+  }),
 }
