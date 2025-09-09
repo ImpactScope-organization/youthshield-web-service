44a680392eea46bf9eb6eff2761c3a6bfc9210d9
balazs-impact-scope <balazs@impactscope.com>
2025-09-09 07:08:44 +0200

    id

--- src/app/influencers/[id]/page.tsx
+++ src/app/influencers/[id]/page.tsx
@@ -1,7 +1,10 @@
 import { PageContainer } from '@/components/common/PageContainer'
 import { serverClient } from '@/trpc/client/server-client'
 
-export default async () => {
-  const influencer = await serverClient.influencers.get({ id: '68b9722b72066ed02955cb84'})
+type Props = {
+  params: { id: string }
+}
+export default async ({ params: { id } }: Props) => {
+  const influencer = await serverClient.influencers.get({ id })
   return <PageContainer>{influencer?.name}</PageContainer>
 }
